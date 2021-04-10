d77d1f925c3a2760b1fda9af9abed5c8c5eef8bf
Rajas1312 <you@example.com>
2021-04-10 21:21:19 +0530

    reactor: outline icons and adding icons in addnote

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,8 @@ export const environment = {
   loginURL: 'http://localhost:3000/login',
   registerURL: 'http://localhost:3000/user',
   forgotURL: 'http://localhost:3000/forgotPassword',
-  resetURL: 'http://localhost:3000/resetPassword'
+  resetURL: 'http://localhost:3000/resetPassword',
+  addnoteURL: 'http://localhost:3000/notes'
 };
 
 /*
